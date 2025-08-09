4e8297b0d4b400f27914c899e23c2e8697f1be22
Tiagoxlsx <tsaraujo@outlook.com.br>
2025-08-09 16:21:30 +0000

    creiei printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #include <string.h>
+// Definido a constante para o tamanho das strings 
+#define MAX_STRING_SIZE 50
 
+// Estrutura para representar uma carta do super trunfo
 typedef struct {
-    char nome[30];
-    int peso;
-    int altura;
-    int populacao;
+    char estado;                    // 'A' a 'H'
+    char codigo [5];                // EX:"S01", "R02" 4 caracteres + 1 para o terminador null '\0'
+    char nome[MAX_STRING_SIZE];     // Nome da Cidade
+    int populacao;                  // Numero de Habitantes 
+    float Área;                       // Área em Km
+    float PIB;                  // Produto Interno Bruto
+    int num_pontos_turisticos;      // Numero de pontos turisticos 
 } Carta;
 
 void mostrarCarta(Carta c) {
-    printf("Cidade: %s\n", c.nome);
-    printf("1) Peso: %d\n", c.peso);
-    printf("2) Altura: %d\n", c.altura);
-    printf("3) População: %d\n", c.populacao);
+    printf("1) Estado: %s\n", c.nome);
+    printf("2) Codigo: %s\n", c.nome);
+    printf("3) Cidade: %s\n", c.nome);
+    printf("4) População: %d\n", c.populacao);
+    printf("5) PIB: %d\n", c.PIB);
+    printf("6) Área: %d\n", c.Área);
+    printf("7) num_pontos_turisticos; %d\n", c.num_pontos_turisticos);
 }
 
 int main() {
-    Carta saoPaulo = {"São Paulo", 100, 760, 12300000};
-    Carta rio = {"Rio de Janeiro", 95, 600, 6740000};
+    Carta saoPaulo = {'S',"SP01","São Paulo",12325000,699.28,1521,11, 50};
+    Carta rio = {'R', "R02" "Rio de Janeiro", 6748000, 300.50,1200.25, 30};
 
     int escolha;
 
@@ -36,8 +45,8 @@ int main() {
     mostrarCarta(rio);
 
     // Usando ponteiros para evitar repetição
-    int atributosSP[] = {saoPaulo.peso, saoPaulo.altura, saoPaulo.populacao};
-    int atributosRJ[] = {rio.peso, rio.altura, rio.populacao};
+    int atributosSP[] = {saoPaulo.populacao, saoPaulo.Área,saoPaulo.PIB, saoPaulo.num_pontos_turisticos};
+    int atributosRJ[] = {rio.populacao, rio.Área,rio.PIB, rio.num_pontos_turisticos};
 
     int valorSP = atributosSP[escolha - 1];
     int valorRJ = atributosRJ[escolha - 1];
